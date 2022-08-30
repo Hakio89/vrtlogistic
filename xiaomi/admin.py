40546a944f1be5462131b212fcc4fb8e59a0aada
Pawel Kaczmarek <pawel89@windowslive.com>
2022-08-30 05:45:20 +0200

    New templates added

--- xiaomi/admin.py
+++ xiaomi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from xiaomi.models import Xiaomi, Status
 # Register your models here.
+
+admin.site.register(Xiaomi)
+admin.site.register(Status)
--- xiaomi/models.py
+++ xiaomi/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+
+class Xiaomi(models.Model):
+    
+    delivery = models.CharField(max_length=50, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey("Status", on_delete=models.CASCADE)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, 
+                          editable=False)
+    
+    def __str__(self):
+        return str(self.delivery)
+    
+class Status(models.Model):
+    
+    status = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return str(self.status)
+    
