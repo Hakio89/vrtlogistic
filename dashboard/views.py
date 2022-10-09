9d0f215ee07e784120af0ea23c3fbe798855521b
Pawel Kaczmarek <pawel89@windowslive.com>
2022-10-09 14:43:29 +0200

    Add login rewuired on dashboard page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from xiaomi.models import Xiaomi
 
 # Create your views here.
+@login_required
 def dashboard(request):
     deliveries = Xiaomi.objects.all()
     
