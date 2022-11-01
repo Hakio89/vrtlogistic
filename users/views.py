cbdd76f41cc3340364ec5ccb0d8bcde6e12e7cde
Pawel Kaczmarek <pawel89@windowslive.com>
2022-11-01 08:37:49 +0100

    Error validations added

--- users/views.py
+++ users/views.py
@@ -9,63 +9,73 @@ from django.contrib import messages
 def user_login(request):
     
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
         try:
-            user = User.objects.get(username=username)
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            try:
+                user = User.objects.get(username=username)
+            except:
+                messages.warning(request, 'Login is incorrect')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'successfully logged in')
+                return redirect('dashboard')
+            else:
+                messages.warning(request, 'Password is incorrect')
+            
         except:
-            messages.warning(request, 'Login is incorrect')
-        
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'successfully logged in')
-            return redirect('dashboard')
-        else:
-            messages.warning(request, 'Password is incorrect')
+            messages.warning(request, 'Something went wrong. Please contact admin')
         
         
     return render(request, "users/login.html")
 
 def user_logout(request):
-    logout(request)
-    messages.success(request, 'successfully logged out')
+    try:
+        logout(request)
+        messages.success(request, 'successfully logged out')
+        
+    except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
+            
     return redirect('user_login')
 
 def user_registration(request):
     registered = False
     
     if request.method=="POST":
-        
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-        
-        if user_form.is_valid() and profile_form.is_valid():
+        try:
+            user_form = UserForm(data=request.POST)
+            profile_form = UserProfileForm(data=request.POST)
             
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            messages.success(request, 'successfully registered')
-            
-            profile = profile_form.save(commit=False)
-            profile.owner = user
-            
-            if 'profile_image' in request.FILES:
+            if user_form.is_valid() and profile_form.is_valid():
                 
-                profile.profile_image = request.FILES['profile_image']
+                user = user_form.save()
+                user.set_password(user.password)
+                user.save()
+                messages.success(request, 'successfully registered')
+                
+                profile = profile_form.save(commit=False)
+                profile.owner = user
+                
+                if 'profile_image' in request.FILES:
+                    
+                    profile.profile_image = request.FILES['profile_image']
+                
+                profile.save()
+                registered = True    
+                
+            else:
+                print(user_form.errors, profile_form.errors)
+                
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
             
-            profile.save()
-            registered = True            
-            
-            
-        else:
-            print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
         profile_form = UserProfileForm
-    
-        print("Wysłano formularz")
     
     ctx = {"user_form" : user_form,
            "profile_form" : profile_form,
@@ -81,21 +91,26 @@ def user_profile(request, pk):
     profile_image_form = UserProfileImageUpdateForm(instance=user_profile)
     
     if request.method == "POST":
-        profile_image_form = UserProfileImageUpdateForm(request.POST, request.FILES, instance=user_profile)
-        profile_update_form = UserProfileUpdateForm(request.POST, instance=user_profile)
-        
-        if "image_update" in request.POST:
-            user_profile.profile_image.delete()
-            if profile_image_form.is_valid():
-                profile_image_form.save()
-                messages.success(request, 'image successfully updated')
-                return redirect('user_profile', pk )
+        try:
+            profile_image_form = UserProfileImageUpdateForm(request.POST, request.FILES, instance=user_profile)
+            profile_update_form = UserProfileUpdateForm(request.POST, instance=user_profile)
             
-        if "data_update" in request.POST:
-            if profile_update_form.is_valid():
-                profile_update_form.save()     
-                messages.success(request, 'successfully updated')       
-                return redirect('user_profile', pk )
+            if "image_update" in request.POST:
+                user_profile.profile_image.delete()
+                if profile_image_form.is_valid():
+                    profile_image_form.save()
+                    messages.success(request, 'image successfully updated')
+                    return redirect('user_profile', pk )
+                
+            if "data_update" in request.POST:
+                if profile_update_form.is_valid():
+                    profile_update_form.save()     
+                    messages.success(request, 'successfully updated')       
+                    return redirect('user_profile', pk )
+                
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
+        
     ctx = {
         "user" : user,
         "profile_update_form" : profile_update_form,
--- xiaomi/views.py
+++ xiaomi/views.py
@@ -13,41 +13,45 @@ from .utils.tables import Table
 # Create your views here.
 
 @login_required
-def xiaomi(request):
+def xiaomi(request):    
     claims = XiaomiClaimParts.objects.all()
     parts_all = XiaomiPartsCatalog.objects.all()
     waiting_all = XiaomiWaitingParts.objects.all()
     deliveries = Xiaomi.objects.all()
     
     if request.method == "POST":
-        emails = MailReportReceivers.objects.all()
-        deliveries = Xiaomi.objects.all()
-        parts = XiaomiPartsCatalog.objects.get()
-        waiting = XiaomiWaitingParts.objects.get()
-        
-        table = Table(
-                        delivery=deliveries, 
-                        parts=parts,
-                        waiting=waiting,
-                    )
-        
-        report = table.mail_report()
-        
-        ctx = {
-            'report' : report
-        }
-        subject = 'XIAOMI - Aktualny raport oczekujących dostaw'
-        message = get_template('xiaomi/xiaomi-delivery-report.html').render(ctx)
-        msg = EmailMessage(
-            subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            list(emails),
-        )
-        msg.content_subtype ="html"# Main content is now text/html
-        msg.send()
-        messages.success(request, 'report successfully send')
-        
+        try:        
+            emails = MailReportReceivers.objects.all()
+            deliveries = Xiaomi.objects.all()
+            parts = XiaomiPartsCatalog.objects.get()
+            waiting = XiaomiWaitingParts.objects.get()
+            
+            table = Table(
+                            delivery=deliveries, 
+                            parts=parts,
+                            waiting=waiting,
+                        )
+            
+            report = table.mail_report()
+            
+            ctx = {
+                'report' : report
+            }
+            subject = 'XIAOMI - Aktualny raport oczekujących dostaw'
+            message = get_template('xiaomi/xiaomi-delivery-report.html').render(ctx)
+            msg = EmailMessage(
+                subject,
+                message,
+                settings.EMAIL_HOST_USER,
+                list(emails),
+            )
+            msg.content_subtype ="html"# Main content is now text/html
+            msg.send()
+            messages.success(request, 'report successfully send')
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
+
+    
     ctx = {"title" : "Xiaomi Default",
            "claims" : claims,
            "parts" : parts_all,
@@ -63,17 +67,20 @@ def xiaomi_delivery_new(request):
     
     
     if request.method == "POST":
-        form = XiaomiNewForm(request.POST, request.FILES)
-        
-        if form.is_valid():
-            delivery = form.save(commit=False)
-            if delivery.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
-                delivery.creator = user
-                delivery.save()
-                messages.success(request, 'new delivery successfully created')
-                return redirect('xiaomi_deliveries')
-            else:
-                messages.warning(request, 'You are trying o add the wrong file format')
+        try:
+            form = XiaomiNewForm(request.POST, request.FILES)
+            
+            if form.is_valid():
+                delivery = form.save(commit=False)
+                if delivery.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
+                    delivery.creator = user
+                    delivery.save()
+                    messages.success(request, 'new delivery successfully created')
+                    return redirect('xiaomi_deliveries')
+                else:
+                    messages.warning(request, 'You are trying o add the wrong file format')
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
     
     ctx = {
         "title" : "Xiaomi New Delivery",
@@ -87,12 +94,16 @@ def xiaomi_delivery_update(request, pk):
     form = XiaomiDeliveryForm(instance=delivery_update)
     
     if request.method == "POST":
-        form = XiaomiDeliveryForm(request.POST, instance=delivery_update)
-        
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'delivery successfully updated')
-            return redirect('xiaomi_deliveries')
+        try:
+            form = XiaomiDeliveryForm(request.POST, instance=delivery_update)
+            
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'delivery successfully updated')
+                return redirect('xiaomi_deliveries')
+            
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
     
     ctx = {
         'title' : "Xiaomi Delivery Update",
@@ -108,17 +119,21 @@ def xiaomi_delivery_file_update(request, pk):
     form = XiaomiDeliveryFileForm(instance=delivery_update)
     
     if request.method == "POST":
-        delivery_update.file.delete()
-        form = XiaomiDeliveryFileForm(request.POST, request.FILES, instance=delivery_update)
-        
-        if form.is_valid():
-            delivery = form.save(commit=False)
-            if delivery.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
-                delivery.save()
-                messages.success(request, 'file successfully updated')
-                return redirect('xiaomi_deliveries')
-            else:
-                messages.warning(request, 'You are trying o add the wrong file format')
+        try:
+            delivery_update.file.delete()
+            form = XiaomiDeliveryFileForm(request.POST, request.FILES, instance=delivery_update)
+            
+            if form.is_valid():
+                delivery = form.save(commit=False)
+                if delivery.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
+                    delivery.save()
+                    messages.success(request, 'file successfully updated')
+                    return redirect('xiaomi_deliveries')
+                else:
+                    messages.warning(request, 'You are trying o add the wrong file format')
+                    
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
             
     
     ctx = {
@@ -191,11 +206,15 @@ def xiaomi_delivery_delete(request, pk):
     single_delivery_file = single_delivery.file
     
     if request.method == 'POST':
-        single_delivery_file.delete()
-        if request.method == 'POST':
-            single_delivery.delete()
-            messages.success(request, 'delivery successfully removed')
-            return redirect('xiaomi_deliveries')
+        try:
+            single_delivery_file.delete()
+            if request.method == 'POST':
+                single_delivery.delete()
+                messages.success(request, 'delivery successfully removed')
+                return redirect('xiaomi_deliveries')
+        
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
     
     
     ctx = {
@@ -226,12 +245,16 @@ def xiaomi_claims_new(request):
     form = XiaomiClaimForm()
     
     if request.method == "POST":
-        form = XiaomiClaimForm(request.POST)
+        try:
+            form = XiaomiClaimForm(request.POST)
+            
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'claim successfully created')
+                return redirect('xiaomi_claims')
         
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'claim successfully created')
-            return redirect('xiaomi_claims')
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
     
     ctx = {"title" : "Xiaomi Claims New",
            "form" : form,
@@ -244,12 +267,16 @@ def xiaomi_claims_update(request, pk):
     form = XiaomiClaimForm(instance=claim)
     
     if request.method == "POST":
-        form = XiaomiClaimForm(request.POST, instance=claim)
+        try:
+            form = XiaomiClaimForm(request.POST, instance=claim)
+            
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'claim successfully updated')
+                return redirect('xiaomi_claims')
         
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'claim successfully updated')
-            return redirect('xiaomi_claims')
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
     
     ctx = {"title" : "Xiaomi Claims Update",
            "form" : form,
@@ -262,9 +289,13 @@ def xiaomi_claims_delete(request, pk):
     claim = XiaomiClaimParts.objects.get(id=pk)
     
     if request.method == "POST":
-        claim.delete()
-        messages.success(request, 'claim successfully removed')
-        return redirect('xiaomi_claims')
+        try:
+            claim.delete()
+            messages.success(request, 'claim successfully removed')
+            return redirect('xiaomi_claims')
+    
+        except:
+                messages.warning(request, 'Something went wrong. Please contact admin')
     
     ctx = {"title" : "Xiaomi Claims Update",
            "claim" : claim,
@@ -301,17 +332,21 @@ def xiaomi_waiting_update(request, pk):
     form = XiaomiWaitingForm(instance=waiting)
     
     if request.method == "POST":
-        waiting.file.delete()
-        form = XiaomiWaitingForm(request.POST, request.FILES, instance=waiting)       
+        try:
+            waiting.file.delete()
+            form = XiaomiWaitingForm(request.POST, request.FILES, instance=waiting)       
+            
+            if form.is_valid():
+                waiting_file = form.save(commit=False)
+                if waiting_file.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
+                    waiting_file.save()
+                    messages.success(request, 'file successfully updated')
+                    return redirect('xiaomi_waiting')
+                else:
+                    messages.warning(request, 'You are trying o add the wrong file format')
         
-        if form.is_valid():
-            waiting_file = form.save(commit=False)
-            if waiting_file.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
-                waiting_file.save()
-                messages.success(request, 'file successfully updated')
-                return redirect('xiaomi_waiting')
-            else:
-                messages.warning(request, 'You are trying o add the wrong file format')
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
         
     ctx = {"title" : "Xiaomi Waiting Update",
            "form" : form,
@@ -341,34 +376,43 @@ def xiaomi_parts_update(request, pk):
     form = XiaomiPartsForm(instance=parts)
     
     if request.method == "POST":
-        parts.file.delete()
-        form = XiaomiPartsForm(request.POST, request.FILES, instance=parts)       
-        
-        if form.is_valid():
-            parts_file = form.save(commit=False)
-            if parts_file.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
-                parts_file.save()
-                messages.success(request, 'file successfully updated')
-                return redirect('xiaomi_parts')
-            else:
-                messages.error(request, 'Your file has float values. Please change your excel data into general data')
+        try:
+            parts.file.delete()
+            form = XiaomiPartsForm(request.POST, request.FILES, instance=parts)       
+            
+            if form.is_valid():
+                parts_file = form.save(commit=False)
+                if parts_file.file.name.endswith(('.xlsx', '.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
+                    parts_file.save()
+                    messages.success(request, 'file successfully updated')
+                    return redirect('xiaomi_parts')
+                else:
+                    messages.error(request, 'Your file has float values. Please change your excel data into general data')
+            
+        except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
     
     ctx = {"title" : "Xiaomi Parts Update",           
         "form" : form,}
     return render(request, "xiaomi/xiaomi-parts-update.html", ctx)
 
 def xiaomi_delivery_report(request):
-    deliveries = Xiaomi.objects.all()
-    parts = XiaomiPartsCatalog.objects.get()
-    waiting = XiaomiWaitingParts.objects.get()
+    try:
+        deliveries = Xiaomi.objects.all()
+        parts = XiaomiPartsCatalog.objects.get()
+        waiting = XiaomiWaitingParts.objects.get()
+        
+        table = Table(
+                        delivery=deliveries, 
+                        parts=parts,
+                        waiting=waiting,
+                    )
+        
+        report = table.mail_report()
     
-    table = Table(
-                    delivery=deliveries, 
-                    parts=parts,
-                    waiting=waiting,
-                )
-    
-    report = table.mail_report()
+    except:
+            messages.warning(request, 'Something went wrong. Please contact admin')
+            
     ctx = {
         "title" : "Xiaomi Deliveries Report",
         "report" : report,
