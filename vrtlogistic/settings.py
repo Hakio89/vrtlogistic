66be9bff9bc235104e7c4617a558008097162930
Paweł Kaczmarek <ser-pkc@euroccs.local>
2023-05-04 21:11:17 +0200

    Removing ingored file including setting file

--- vrtlogistic/settings.py
+++ vrtlogistic/settings.py
@@ -95,6 +95,18 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
+    "ccs": {
+    "ENGINE": "mssql",
+    "NAME": "DAPS",
+    "USER": "django_user",
+    "PASSWORD": "DE3.rLAf=)e6Sej",
+    "HOST": "192.168.3.2\django",
+    "PORT": "1433",
+    "OPTIONS": {
+        "driver": "ODBC Driver 18 for SQL Server",
+        "extra_params": "Encrypt=no",
+        }    
+    },
 }
 
 
