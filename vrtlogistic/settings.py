27109e50bc6a6a4f8e55d7cc24d0ecbeaf86950b
Paweł Kaczmarek <ser-pkc@euroccs.local>
2023-05-09 07:19:26 +0200

    allowed hosts added

--- vrtlogistic/settings.py
+++ vrtlogistic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1', '192.168.2.142']
+ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1', '192.168.2.142', 'virtuallogistic','virtuallogistic.euroccs.local' ]
 
 
 # Application definition
